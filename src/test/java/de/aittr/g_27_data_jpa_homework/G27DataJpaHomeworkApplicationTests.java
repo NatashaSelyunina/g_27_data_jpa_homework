e4d42e7db06b090968f60c38f8fa06ea9ce2bee7
Natalya Selyunina <seluninanv@mail.ru>
2024-02-18 16:45:16 +0300

    init commit

---
+++ src/test/java/de/aittr/g_27_data_jpa_homework/G27DataJpaHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_data_jpa_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G27DataJpaHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
